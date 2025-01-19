7482a901f081307f84318741b66695ab5e314f24
Phylip28 <felipe28769@gmail.com>
2025-01-19 16:19:48 -0500

    crear funcion sortearNombre

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-/* 
-    Funciones
-    
-    4. Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se
-    seleccionará aleatoriamente un nombre de la lista y se mostrará en la 
-    página.
-*/
-
 let listaDeNombres = [];
 let ingresarNombre = "";
+let indiceGanador = "";
 
 function agregarNombres(nombre) {
     return listaDeNombres.push(nombre);
@@ -27,9 +20,19 @@ function validarNombre(nombre) {
 function verLista() {
     return listaDeNombres;
 }
+
+function sortearNombre(lista) {
+    if (lista.length == 0){
+        return alert("");
+    } else {
+        indiceGanador = Math.floor(Math.random()*lista.length);
+        return listaDeNombres[indiceGanador];
+    }
+}
 /*
 
-3. Visualizar la lista: Los nombres ingresados aparecerán en una lista
-debajo del campo de entrada.
+4. Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se
+    seleccionará aleatoriamente un nombre de la lista y se mostrará en la 
+    página.
 
 */
